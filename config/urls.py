d512c5567bf8411e54bb34794ec8f10644f3256b
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-08-10 23:48:26 +0300

    chore: configure root URL routing with media and debug toolbar

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.blog.urls')),
+    path('', include('apps.accounts.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += debug_toolbar_urls()
