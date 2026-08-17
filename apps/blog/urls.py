a5724055096adcaad88af51e7f668240a0a7e40e
Mikhail Isaykin <252649173+mikhail-isaykin@users.noreply.github.com>
2026-08-17 23:55:16 +0300

    feat: add post by tag url and reformat blog imports

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from .views import PostCreateView, PostDetailView, PostFromCategory, PostListView, PostUpdateView, comment_create
+from .views import (
+    PostByTagListView,
+    PostCreateView,
+    PostDetailView,
+    PostFromCategory,
+    PostListView,
+    PostUpdateView,
+    comment_create,
+)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
@@ -9,4 +17,5 @@ urlpatterns = [
     path('detail/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
     path('category/<slug:slug>/', PostFromCategory.as_view(), name='post_from_category'),
     path('post/<int:post_id>/comment/', comment_create, name='comment_create'),
+    path('post/tags/<slug:tag>/', PostByTagListView.as_view(), name='post_by_tags'),
 ]
